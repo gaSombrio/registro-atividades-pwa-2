fc14411d8c9bdab1b4c2070be011cc6eb9b7958b
gaSombrio <sombrio.ga@gmail.com>
2026-04-29 08:32:40 -0300

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        cleanupOutdatedCaches: true,
+        sourcemap: false,
+      },
+      manifest: {
+        name: 'Gerenciador de Tarefas',
+        short_name: 'Tarefas',
+        description: 'Aplicativo PWA para gerenciar tarefas diárias',
+        theme_color: '#4a90d9',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
+});
