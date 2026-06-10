863468daacdb08effe0e298d88adff765706eca6
gaSombrio <sombrio.ga@gmail.com>
2026-06-10 09:30:28 -0300

    fix: corrigir caminho de importação do store de autenticação

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -39,7 +39,7 @@
 <script setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
-import { useAuthStore } from '@/stores/auth';
+import { useAuthStore } from '../stores/auth';
 
 const router = useRouter();
 const authStore = useAuthStore();
